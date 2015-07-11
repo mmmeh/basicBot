dd71a7ce50689999763de00fd064cb119df556e3
mmmeh <davidcrowe96@gmail.com>
2015-07-11 15:38:39 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2959,6 +2959,7 @@
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         if (typeof basicBot.settings.rulesLink === "string")
+                            API.sendChat('/me hi');
                             return API.sendChat(subChat(basicBot.chat.roomrules, {link: basicBot.settings.rulesLink}));
                     }
                 }
